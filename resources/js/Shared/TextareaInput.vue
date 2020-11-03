74688c2275015feccb576ea3f848a39033cafcd1
zendzo <zendzzo@gmail.com>
2020-11-03 20:14:45 +0700

    add TextareaInput shared component

---
+++ resources/js/Shared/TextareaInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <label v-if="label" :for="id">{{ label }}:</label>
+    <textarea
+      :id="id"
+      ref="input"
+      v-bind="$attrs"
+      class="form-control"
+      :class="{ error: error }"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+      :placeholder="label"
+    />
+    <div v-if="error" class="form-error">{{ error }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    id: {
+      type: String,
+    },
+    value: String,
+    label: String,
+    error: String,
+    placeholder: String,
+  },
+  methods: {
+    focus() {
+      this.$refs.input.focus();
+    },
+    select() {
+      this.$refs.input.select();
+    },
+  },
+};
+</script>
