8f0472faf116a904b6caeb24ab7fc94c342fe584
zendzo <zendzzo@gmail.com>
2020-11-03 20:13:15 +0700

    RemainderController@create

--- app/Http/Controllers/RemainderController.php
+++ app/Http/Controllers/RemainderController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lead;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class RemainderController extends Controller
 {
-    //
+    public function create(Lead $lead)
+    {
+        return Inertia::render('Remainder/Create',[
+            'lead' => $lead
+        ]);
+    }
 }
