123d4e504bef575178204bb46c6c154fb6f743a7
zendzo <zendzzo@gmail.com>
2020-11-03 23:29:20 +0700

    remainder store and edit

--- app/Http/Controllers/RemainderController.php
+++ app/Http/Controllers/RemainderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Lead;
+use App\Models\Remainder;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -12,6 +13,32 @@ class RemainderController extends Controller
     {
         return Inertia::render('Remainder/Create',[
             'lead' => $lead
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+       $postData =  $this->validate($request,[
+            'remainder' => 'required|min:2',
+            'lead_id' => 'required|exists:leads,id',
+            'remainder_date' => 'required|date'
+        ]);
+
+        $postData['user_id'] = $request->user()->id;
+        $postData['status'] = 'pending';
+
+        $lead = Lead::findOrfail($request->lead_id);
+        $lead->remainders()
+            ->create($postData);
+        
+        return redirect()->route('lead.edit', $lead);
+    }
+
+    public function edit(Lead $lead, Remainder $remainder)
+    {
+        return Inertia::render('Remainder/Edit',[
+            'lead' => $lead,
+            'remainder' => $remainder
         ]);
     }
 }
