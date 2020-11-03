699f9182aee8169f89f61419195d03623c6a6011
zendzo <zendzzo@gmail.com>
2020-11-03 23:28:38 +0700

    wraping route

--- routes/web.php
+++ routes/web.php
@@ -9,14 +9,16 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => 'auth'], function(){
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
     Route::get('/lead', [LeadController::class, 'index'])->name('lead.index');
     Route::get('/lead/create', [LeadController::class, 'create'])->name('lead.create');
     Route::post('/lead/store', [LeadController::class, 'store'])->name('lead.store');
     Route::get('/lead/edit/{lead}', [LeadController::class, 'edit'])->name('lead.edit');
-
     Route::patch('/lead/update/{lead}', [LeadController::class, 'update'])->name('lead.update');
 
     Route::get('/lead/{lead}/remainder/create', [RemainderController::class,'create'])->name('remainder.create');
+    Route::post('/lead/remainder/store', [RemainderController::class, 'store'])->name('remainder.store');
+    Route::get('/lead/{lead}/remainder/{remainder}', [RemainderController::class,'edit'])->name('remainder.edit');
     
 });
 
