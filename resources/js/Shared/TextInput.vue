93c3d1ee6835f43f1d5a62797af32e7b2204a508
zendzo <zendzzo@gmail.com>
2020-11-03 16:48:07 +0700

    recaftor text-input to vue component to catch validation

---
+++ resources/js/Shared/TextInput.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="form-group">
+    <label v-if="label" :for="id">{{ label }}:</label>
+    <input
+      :id="id"
+      ref="input"
+      v-bind="$attrs"
+      class="form-control"
+      :class="{ 'is-invalid': error }"
+      :type="type"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+      :placeholder="label"
+    />
+    <div v-if="error" class="invalid-feedback">
+      {{ error }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    id: {
+      type: String,
+    },
+    type: {
+      type: String,
+      default: "text",
+    },
+    value: String,
+    label: String,
+    placeholder: String,
+    error: String,
+  },
+  methods: {
+    focus() {
+      this.$refs.input.focus();
+    },
+    select() {
+      this.$refs.input.select();
+    },
+    setSelectionRange(start, end) {
+      this.$refs.input.setSelectionRange(start, end);
+    },
+  },
+};
+</script>
